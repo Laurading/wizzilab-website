1f88952599f44ce45121fead69bd29ab4f12bb62
Laurading <Laurxding@hotmail.com>
2022-06-07 16:20:16 +0200

    solutions

---
+++ pages/wizzipark.js
@@ -0,0 +1,108 @@
+import Header from '../components/Header'
+
+
+export default function Wizzipark() {
+    return (
+        <div>
+            <Header />
+            <section className='section-one anim'>
+
+            </section>
+            <section className='margin'>
+            <h2>Un système en temps réel et autonome</h2>
+            <p>
+            Des capteurs de stationnement sans fils et autonomes détectent instantanément l’arrivée ou le départ de véhicules et relaient l’information vers le Wizzicloud au travers de la gateway. Les visiteurs ont accès en temps réel aux places disponibles qui peuvent être communiquées sur des afficheurs dynamiques.
+            </p>
+            <p>
+                <span>2 types de capteurs </span>
+                sont disponibles :
+            </p>
+            <ul>
+            <li>
+            Le <span>capteur magnétique</span>, se fixant au sol, détectant la présence d'un véhicule.
+            </li>
+            <li>
+                Le <span>capteur infrarouge à LED</span>, se fixant au plafond, change de couleur en fonction du statut de la place et au cas d'usage.
+            </li>
+            </ul>
+                <img className='solutions-img' src='/img/solutions/wizzipark.png' />
+            </section>
+       
+            <section className='lavender-bg'>
+                <div className='margin'>
+                <h2>Vos avantages</h2>
+                <div className='content-col'>
+                    <div className='column-3'>
+                        <img className='picto' src='/img/pictos/wizzipark/1.svg'/>
+                        <p>Améliorer le processus d’accueil et la satisfaction des conducteurs</p>
+                    </div>
+                    <div className='column-3'>
+                        <img className='picto' src='/img/pictos/wizzipark/2.svg'/>
+                        <p>Identifier les voitures ventouses</p>
+                    </div>
+                    <div className='column-3'>
+                        <img className='picto' src='/img/pictos/wizzipark/3.svg'/>
+                        <p>Optimiser le taux de remplissage de votre parking</p>
+                    </div>
+                    <div className='column-3'>
+                        <img className='picto' src='/img/pictos/wizzipark/4.svg'/>
+                        <p>Exploiter un système de réservation de places de parking</p>
+                    </div>
+                    <div className='column-3'>
+                        <img className='picto' src='/img/pictos/wizzipark/5.svg'/>
+                        <p>Informer les visiteurs sur les emplacements spécifiques du parking</p>
+                    </div>
+                    <div className='column-3'>
+                        <img className='picto' src='/img/pictos/wizzipark/6.svg'/>
+                        <p>Réduire le temps de recherche d’une place de stationnement</p>
+                    </div>
+                </div>
+                </div>
+            </section>
+            <section className='margin'>
+                <h2>Nos composants</h2>
+
+                <div className='content-col'>
+                    <div className='column-4'>
+                        <img src='/img/composants/wizzipark/1.png'/>
+                        <h4>Capteur magnétique</h4>
+                        <ul>
+                        <li>Identifie la présence d’un véhicule</li>
+                        <li>Envoie l’information de présence à la gateway</li>
+                        </ul>
+                    </div>
+                    <div className='column-4'>
+                    <img src='/img/composants/wizzipark/2.png'/>
+                        <h4>Capteur infrarouge à LED</h4>
+                        <ul>
+                            <li>Identifie la présence d’un véhicule</li>
+                            <li>Communique le statut de la place via le LED RGBy</li>
+                            <li>Envoie l’information de présence à la gateway</li>
+                        </ul>
+                    </div>
+                    <div className='column-4'>
+                    <img src='/img/composants/wizzipark/3.png'/>
+                         <h4>Gateway</h4>
+                         <ul>
+                             <li>Passerelle</li>
+                             <li>Récolte les informations des capteurs</li>
+                             <li>Envoie les informations au cloud</li>
+                         </ul>
+                    </div>
+                    <div className='column-4'>
+                    <img src='/img/composants/wizzipark/4.png'/>
+                        <h4>Cloud</h4>
+                        <ul>
+                            <li>Récupère les informations par la gateway</li>
+                            <li>Affiche les informations sur les places</li>
+                            <li>Permet de configurer les capteurs et la gateway</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <section className='section-one anim'>
+           
+        </section>
+        </div>
+    )
+}
---
+++ pages/wolt.js
@@ -0,0 +1,105 @@
+import Header from '../components/Header'
+
+
+export default function Wolt() {
+    return (
+        <div>
+            <Header />
+            <section className='section-one anim'>
+
+            </section>
+            <section className='margin'>
+            <h2>Un systeme de detection et d'alerte</h2>
+            <p>
+                WOLT se compose de tags, d’ancres et de gateways. Le tag (capteur) se localise par rapport aux ancres à une fréquence prédéfinie en fonction du cas d’usage. La donnée est envoyée dans le cloud à travers la gateway.
+            </p>
+            <p>
+                <span>3 niveaux de précisions de localisation </span>
+                sont possibles :
+            </p>
+            <ul>
+                <li>Le <span>positionnement</span>, le plus précis des 3 niveaux, qui permet d’identifier la position exacte de l’objet</li>
+                <li>La <span>proximité</span> qui permet de connaître la position approximative d’un tag par rapport aux ancres</li>
+                <li>La <span>présence</span> qui permet de savoir si un objet dans une zone d’intérêt</li>
+            </ul>
+                <img className='solutions-img' src='/img/solutions/wolt.png' />
+            </section>
+       
+            <section className='lavender-bg'>
+                <div className='margin'>
+                <h2>Vos avantages</h2>
+                <div className='content-col'>
+                    <div className='column-3'>
+                        <img className='picto' src='/img/pictos/wolt/1.svg'/>
+                        <p>Localiser les objets / inventaires et diminuer les pertes</p>
+                    </div>
+                    <div className='column-3'>
+                        <img className='picto' src='/img/pictos/wolt/2.svg'/>
+                        <p>Installer facilement et rapidement notre kit</p>
+                    </div>
+                    <div className='column-3'>
+                        <img className='picto' src='/img/pictos/wolt/3.svg'/>
+                        <p>Identifier les points de congestion et réduire et temps mort</p>
+                    </div>
+                    <div className='column-3'>
+                        <img className='picto' src='/img/pictos/wolt/4.svg'/>
+                        <p>Adapter les besoins de localisation aux différents cas d’usage</p>
+                    </div>
+                    <div className='column-3'>
+                        <img className='picto' src='/img/pictos/wolt/5.svg'/>
+                        <p>Optimiser les déplacements de vos objets / itinéraires</p>
+                    </div>
+                    <div className='column-3'>
+                        <img className='picto' src='/img/pictos/wolt/6.svg'/>
+                        <p>Améliorer la performance de votre activité</p>
+                    </div>
+                </div>
+                </div>
+            </section>
+            <section className='margin'>
+                <h2>Nos composants</h2>
+
+                <div className='content-col'>
+                    <div className='column-4'>
+                        <img src='/img/composants/wolt/1.png'/>
+                        <h4>Ancre</h4>
+                        <ul>
+                        <li>Les tags mesurent leur distance avec lui</li>
+                        <li>Sert de repère fix</li>
+                        <li>Composant “passif”</li>
+                        </ul>
+                    </div>
+                    <div className='column-4'>
+                    <img src='/img/composants/wolt/2.png'/>
+                        <h4>Tags</h4>
+                        <ul>
+                            <li>Objet mobile dont on veut connaître la position</li>
+                            <li>Mesure sa distance avec les ancres et envoie l’information à la gateway</li>
+                            <li>Deux types de tags qui permettent deux précisions : la position et la présence</li>
+                        </ul>
+                    </div>
+                    <div className='column-4'>
+                    <img src='/img/composants/wolt/3.png'/>
+                         <h4>Gateway</h4>
+                         <ul>
+                             <li>Passerelle</li>
+                             <li>Récolte les informations des tags et des ancres</li>
+                             <li>Envoie les informations au cloud</li>
+                         </ul>
+                    </div>
+                    <div className='column-4'>
+                    <img src='/img/composants/wolt/4.png'/>
+                        <h4>Cloud</h4>
+                        <ul>
+                            <li>Définit la position du tag en fonction des informations reçues par celui-ci</li>
+                            <li>Permet de configurer les tags et les ancres</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <section className='section-one anim'>
+           
+        </section>
+        </div>
+    )
+}
