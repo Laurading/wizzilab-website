de8b8bf3b04f01285b5be3a20122a86dc803d12d
Laurading <Laurxding@hotmail.com>
2022-06-07 16:19:34 +0200

    scss setup & main

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../styles/setup.scss'
+import '../styles/main.scss'
+import '../styles/header.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
