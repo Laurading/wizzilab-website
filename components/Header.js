a4339e15e9b242b52d7ee5deb438f6bd5d3b2ce9
Laurading <Laurxding@hotmail.com>
2022-06-08 17:52:37 +0200

    header change on scroll

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,32 @@
-
+import React, { useState, useEffect, useRef } from 'react';
 
 const Header = () => {
+    const prevScrollY = useRef(0);
+
+  const [goingUp, setGoingUp] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (prevScrollY.current < currentScrollY && goingUp) {
+        setGoingUp(false);
+      }
+      if (prevScrollY.current > currentScrollY && !goingUp) {
+        setGoingUp(true);
+      }
+
+      prevScrollY.current = currentScrollY;
+      console.log(goingUp, currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [goingUp]);
+    
+    
     return (
-        <header>
+        <header className={goingUp === false ? "onscroll" : ""}>
             <nav className="margin">
                 <img src='/img/logo/wizzilab.png' />
                 <ul>
@@ -16,7 +40,7 @@ const Header = () => {
                         </ul> */}
                     </li>
                     <li>
-                        <a>Ressources</a>
+                        <a>Support</a>
                     </li>
                     <li>
                         <a>A propos</a>
