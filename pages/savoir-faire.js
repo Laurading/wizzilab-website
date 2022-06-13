711520bab48e54428aed0516d242d79a2535f594
Laurading <Laurxding@hotmail.com>
2022-06-13 13:08:24 +0200

    savoir-faire structure

--- pages/savoir-faire.js
+++ pages/savoir-faire.js
@@ -1,13 +1,139 @@
 import Header from '../components/Header'
 
 
-export default function Wolt() {
+export default function SavoirFaire() {
     return (
         <div>
             <Header />
-            <section className='lavender-bg'>
-                
+            <section className='section-one'>
+                <div className='lavender-bg'>
+                    <h2>Notre savoir faire</h2>
+                </div>
             </section>
+            <section className='margin'>
+                <div className='content-col'>
+                    <div className='column-2'>
+                        <h3> Qu'est ce qu'un projet IoT?</h3>
+                        <p>
+                            Wizzilab répond aux besoins à l’aide des nouvelles générations de composants éléctroniques et les nouvelles technologies.
+                        </p>
+                        <p>
+                            Comprendre les défis quotidiens dans les secteurs professionnels, publics ou privés, nous permet d’optimiser les processus, de réduire les risques et d’économiser les ressources.
+                        </p>
+                    </div>
+                    <div className='column-2'>
+                    </div>
+                </div>
+                <p>Pour Wizzilab il y a une opportunité technologique à saisir via :</p>
+                <ul>
+                    <li>
+                        Les infrastructures disponibles, cellulaires et câblées qui sont adaptées pour une consommation d’énergie à haut débit d’application
+                    </li>
+                    <li>
+                        Le monde de l’IoT, capable d’effectuer un travail de manière cohérente pendant des années
+                    </li>
+                </ul>
+                <p>Nous déployons les capacités extraordinaires de l’IoT en maîtrisant des technologies et des systèmes de conception qui viennent répondre à des demandes spécifiques de nos clients.</p>
+            </section>
+
+            <section className='lavender-bg'>
+                <div className='margin'>
+                    <h3>Un système IoT efficace</h3>
+                    <div className='content-col'>
+                        <div className='column-3'>
+                            <img className='picto' src='/img/pictos/u-guard/1.svg' />
+                            <p>Identifier et réduire les situations à risque sur votre site</p>
+                        </div>
+                        <div className='column-3'>
+                            <img className='picto' src='/img/pictos/u-guard/2.svg' />
+                            <p>Installer facilement et rapidement notre kit</p>
+                        </div>
+                        <div className='column-3'>
+                            <img className='picto' src='/img/pictos/u-guard/3.svg' />
+                            <p>Alerter le conducteur sur la proximité d’un engin ou d’un pieton</p>
+                        </div>
+                        <div className='column-3'>
+                            <img className='picto' src='/img/pictos/u-guard/4.svg' />
+                            <p>Améliorer les différents comportements de déplacement</p>
+                        </div>
+                        <div className='column-3'>
+                            <img className='picto' src='/img/pictos/u-guard/5.svg' />
+                            <p>Avertir les pietons lorsqu’ils entrent dans une zone potentiellement risquée</p>
+                        </div>
+                        <div className='column-3'>
+                            <img className='picto' src='/img/pictos/u-guard/6.svg' />
+                            <p>Adapter les paramètres de détection à votre parc d’engins et votre environnement</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='margin'>
+                <h3>
+                    Notre expertise technique
+                </h3>
+                <p>Sur le plan technologique, Wizzilab répond aux demandes de cas d’usages spécifiques grâce à son équipe experte dans les systèmes embarqués basse consommation, technologies IoT sans fil, sécurité et service cloud.</p>
+                <p>
+                    Notre expertise technique comprend :
+                </p>
+                <ul>
+                    <li>
+                        La conception de la carte avec des contraintes RF
+                    </li>
+                    <li>
+                        La création de firmware embarqué à puissance limitée
+                    </li>
+                    <li>
+                        La gestion de technologies IoT sans fil
+                    </li>
+                    <li>
+                        Le traitement efficace des données et algorithmes
+                    </li>
+                    <li>
+                        La séléction de réseaux et services cloud adaptés
+                    </li>
+                    <li>
+                        La certification et industrialisation
+                    </li>
+                    <li>
+                        Le suivi de la production à grande échelle
+                    </li>
+                </ul>
+            </section>
+            <section className='margin'>
+                <h3>Une philosophie de conception</h3>
+                <p>Chez Wizzilab, nous mettons en pratique une philosophie de conception stricte :</p>
+                <div className='content-col'>
+                    <div className='column-3'>
+                        <h5>Développement de systèmes de bout en bout.</h5>
+                        <p>
+                        Nous avons toujours à l’esprit l’ensemble des besoins, le coût et les défis de faible puissance par rapport au système, donc chaque composant est adapté pour fonctionner efficacement avec le reste du système.
+                        </p>
+                    </div>
+                    <div className='column-3'>
+                        <h5>Communication sans fil optimale.</h5>
+                        <p>Nous croyons que la disponibilité du réseau les infrastructures répondant aux exigences à grande échelle et à faible coût de l’IoT est une condition préalable au succès de tout système IoT. Nous avons la possibilité de combiner différentes technologies sans fil comme le DASH7, LoRaWAN et cellulaire entre autres, afin d’optimiser les compromis entre le débit de données, la portée et la consommation d’énergie.</p>
+                    </div>
+                    <div className='column-3'>
+                        <h5>Localisation.</h5>
+                        <p>Le positionnement intérieur et extérieur est au coeur d’une multitude des applications professionnelles et privées.
+Nous avons développé une solide expertise en UWB sans fil, ce qui nous permet d’estimer les distances avec un sous-compteur précision.</p>
+                    </div>
+                    <div className='column-3'>
+                        <h5>Décisions évenementielles en cascade.</h5>
+                        <p>Nous utilisons des déclencheurs de faible puissance pour nous réveiller davantage modes de consommation d’énergie.
+Cela permet de conserver la consommation extrêmement faible, tout en offrant la meilleure valeur fonctionnelle.</p>
+                    </div>
+                    <div className='column-3'>
+                        <h5>Edge computing et algorithmes embarqués.</h5>
+                        <p>Les informations sont traitées et filtrées dans les noeuds périphériques, chaque fois que nécessaire, avec la possibilité de rediriger et les fulx de données agrégés. Cela permet d’optimiser le traiteuement à chaque étape, évite les débordements et ne fournit que des données significatives au service d’application.</p>
+                    </div>
+                    <div className='column-3'>
+                        <h5>Gestion centralisée de la flotte.</h5>
+                        <p>Un système IoT efficace nécessite un moyen simple de servir et de configurer le parc de noeuds d’extrémité IoT. Nous concevons nos systèmes pour garantir qu’ils peuvent être accessibles tout au long de leur vie pour la maintenance, la reconfiguation, la sécurité et la mise à jour du firmware.</p>
+                    </div>
+                </div>
+            </section>
+
         </div>
     )
 }
