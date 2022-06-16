807cc1a632e9b4ffd6d519f28b9bc717a4764f85
Laurading <Laurxding@hotmail.com>
2022-06-16 18:10:29 +0200

    contact fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import '../styles/setup.scss'
 import '../styles/main.scss'
 import '../styles/header.scss'
-import '../styles/responsive.scss'
 import '../styles/popup.scss'
+import '../styles/responsive.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
