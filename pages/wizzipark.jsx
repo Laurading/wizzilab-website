fa9bebe380290c84b88f7ea48e12f21b90b6419f
Laurading <Laurxding@hotmail.com>
2022-06-22 17:57:37 +0200

    text changes

--- pages/wizzipark.jsx
+++ pages/wizzipark.jsx
@@ -8,7 +8,7 @@ export default function Wizzipark() {
             <Header />
             <section className='section-one'>
             <div className='margin-lr'>
-            <h2><span className='red'>WIZZIPARK :</span> La solution de stationnement intelligent et en temps réel</h2>
+            <h2><span className='red'>WIZZIPARK :</span> La solution de stationnement intelligent en temps réel</h2>
             <img className='solutions-img' src='/img/solutions/wizzipark.png' />
             <p>
             Des capteurs de stationnement sans fils et autonomes détectent instantanément l&apos;arrivée ou le départ de véhicules et relaient l&apos;information vers le Wizzicloud au travers de la gateway. Les visiteurs ont accès en temps réel aux places disponibles qui peuvent être communiquées sur des afficheurs dynamiques.
@@ -19,10 +19,10 @@ export default function Wizzipark() {
             </p>
             <ul>
             <li>
-            Le <span>capteur magnétique</span>, se fixant au sol, détectant la présence d&apos;un véhicule.
+            Le <span>capteur au sol</span>, détectant la présence d&apos;un véhicule.
             </li>
             <li>
-                Le <span>capteur infrarouge à LED</span>, se fixant au plafond, change de couleur en fonction du statut de la place et au cas d&apos;usage.
+                Le <span>capteur plafonnier</span>, change de couleur en fonction du statut de la place et au cas d&apos;usage.
             </li>
             </ul>
                 
@@ -35,27 +35,27 @@ export default function Wizzipark() {
                 <div className='content-col'>
                     <div className='column-3 six'>
                         <img className='picto' src='/img/pictos/wizzipark/1.svg'/>
-                        <p className='center'>Améliorer le processus d&apos;accueil et la satisfaction des conducteurs</p>
+                        <p className='center'>Améliore le processus d&apos;accueil et la satisfaction des conducteurs</p>
                     </div>
                     <div className='column-3 six'>
                         <img className='picto' src='/img/pictos/wizzipark/2.svg'/>
-                        <p className='center'>Identifier les voitures ventouses</p>
+                        <p className='center'>Identifie les voitures ventouses</p>
                     </div>
                     <div className='column-3 six'>
                         <img className='picto' src='/img/pictos/wizzipark/3.svg'/>
-                        <p className='center'>Optimiser le taux de remplissage de votre parking</p>
+                        <p className='center'>Optimise le taux de remplissage de votre parking</p>
                     </div>
                     <div className='column-3 six'>
                         <img className='picto' src='/img/pictos/wizzipark/4.svg'/>
-                        <p className='center'>Exploiter un système de réservation de places de parking</p>
+                        <p className='center'>Exploite un système de réservation de places de parking</p>
                     </div>
                     <div className='column-3 six'>
                         <img className='picto' src='/img/pictos/wizzipark/5.svg'/>
-                        <p className='center'>Informer les visiteurs sur les emplacements spécifiques du parking</p>
+                        <p className='center'>Informe les visiteurs sur les emplacements spécifiques du parking</p>
                     </div>
                     <div className='column-3 six'>
                         <img className='picto' src='/img/pictos/wizzipark/6.svg'/>
-                        <p className='center'>Réduire le temps de recherche d&apos;une place de stationnement</p>
+                        <p className='center'>Réduit le temps de recherche d&apos;une place de stationnement</p>
                     </div>
                 </div>
                 </div>
@@ -66,37 +66,42 @@ export default function Wizzipark() {
                 <div className='content-col'>
                     <div className='column-4'>
                         <img src='/img/composants/wizzipark/1.png'/>
-                        <h5 className='red'>Capteur magnétique</h5>
+                        <h5 className='blue'>WP100 : Capteur au sol</h5>
                         <ul>
                         <li>Identifie la présence d&apos;un véhicule</li>
                         <li>Envoie l&apos;information de présence à la gateway</li>
+                        <li>Deux options d'installation : à<span className='cultured'>-</span>coller ou à<span className='cultured'>-</span>encastrer</li>
                         </ul>
                     </div>
                     <div className='column-4'>
                     <img src='/img/composants/wizzipark/2.png'/>
-                        <h5 className='blue'>Capteur infrarouge à LED</h5>
+                        <h5 className='red'>WP200 : Capteur plafonnier</h5>
                         <ul>
                             <li>Identifie la présence d’un véhicule</li>
-                            <li>Communique le statut de la place via le LED RGB</li>
+                            <li>Affiche le statut de la place par retour lumineux</li>
                             <li>Envoie l&apos;information de présence à la gateway</li>
                         </ul>
                     </div>
                     <div className='column-4'>
                     <img src='/img/composants/wizzipark/3.png'/>
-                         <h5 className='green'>Gateway</h5>
+                         <h5 className='green'>WGATE-PRO : Gateway</h5>
                          <ul>
-                             <li>Passerelle</li>
+                             <li>Passerelle IoT</li>
                              <li>Récolte les informations des capteurs</li>
                              <li>Envoie les informations au cloud</li>
+                             <li>Permet les interventions à distance</li>
+                             <li>Deux options de connexion : mobile ou Ethernet</li>
                          </ul>
                     </div>
                     <div className='column-4'>
                     <img src='/img/composants/wizzipark/4.png'/>
-                        <h5 className='yellow'>Cloud</h5>
+                        <h5 className='yellow'>Wizzipark : Cloud</h5>
                         <ul>
-                            <li>Récupère les informations par la gateway</li>
+                            <li>Récupère les informations transitant la / les gateway(s)</li>
                             <li>Affiche les informations sur les places</li>
                             <li>Permet de configurer les capteurs et la gateway</li>
+                            <li>Fournit des statistiques d'activité (nombre de visiteurs, taux d'occupation,...)</li>
+                            <li>L'ensemble de vos parkings en supervision sur une seule interface</li>
                         </ul>
                     </div>
                 </div>
