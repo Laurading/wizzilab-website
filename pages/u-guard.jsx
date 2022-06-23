f4ee498c2e3b996376975c7133ee39ef9e7417a9
Laurading <Laurxding@hotmail.com>
2022-06-23 18:04:33 +0200

    fix

--- pages/u-guard.jsx
+++ pages/u-guard.jsx
@@ -1,108 +1,108 @@
 // import Header from '../components/Header'
 // import Footer from '../components/Footer'
 
-// export default function Uguard() {
-//     return (
-//         <div>
-//             <Header />
-//             <section className='section-one'>
-//             <div className='margin-lr'>
-//             <h2><span className='orange'>U-GUARD : </span> La solution &ldquo;Proximity&rdquo;</h2>
-//                 <img className='solutions-img' src='/img/solutions/u-guard.png' />
-//                 <p>5 capteurs (4 U-Guard-P et 1 U-Guard-M) génèrent un périmètre de sécurité autour de l&apos;engin afin de détecter la proximité des piétons ou d’autres engins (via un badge ou un système identique).</p>
-//                 <p>Il existe 
-//                     <span> 2 zones de sécurité </span>
-//                 controlées par le U-Guard-M :</p>
-//                 <ul>
-//                     <li>
-//                         La
-//                             <span className='yellow'> zone de prévention </span>
-//                         qui informe le conducteur de la présence d&apos;autrui dans son espace de travail non visible.
-//                     </li>
-//                     <li>
-//                         La
-//                             <span className='red'> zone de danger </span>
-//                         qui alerte les deux protagonistes d&apos;un danger de collision immédiat.
-//                     </li>
-//                 </ul>
-//             </div>
-                
-//             </section>
-       
-//             <section className='lavender-bg'>
-//                 <div className='margin'>
-//                     <h3>Vos avantages</h3>
-//                     <div className='content-col'>
-//                         <div className='column-3 six'>
-//                             <img className='picto' src='/img/pictos/u-guard/1.svg'/>
-//                             <p className='center'>Identifier et réduire les situations à risque sur votre site</p>
-//                         </div>
-//                         <div className='column-3 six'>
-//                             <img className='picto' src='/img/pictos/u-guard/2.svg'/>
-//                             <p className='center'>Installer facilement et rapidement notre kit</p>
-//                         </div>
-//                         <div className='column-3 six'>
-//                             <img className='picto' src='/img/pictos/u-guard/3.svg'/>
-//                             <p className='center'>Alerter le conducteur sur la proximité d&apos;un engin ou d&apos;un pieton</p>
-//                         </div>
-//                         <div className='column-3 six'>
-//                             <img className='picto' src='/img/pictos/u-guard/4.svg'/>
-//                             <p className='center'>Améliorer les différents comportements de déplacement</p>
-//                         </div>
-//                         <div className='column-3 six'>
-//                             <img className='picto' src='/img/pictos/u-guard/5.svg'/>
-//                             <p className='center'>Avertir les pietons lorsqu&apos;ils entrent dans une zone potentiellement risquée</p>
-//                         </div>
-//                         <div className='column-3 six'>
-//                             <img className='picto' src='/img/pictos/u-guard/6.svg'/>
-//                             <p className='center'>Adapter les paramètres de détection à votre parc d&apos;engins et votre environnement</p>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </section>
-//             <section className='margin'>
-//                 <h3>Nos composants</h3>
+export default function Uguard() {
+    return (
+        <div>
+            <Header />
+            {/* <section className='section-one'>
+                <div className='margin-lr'>
+                    <h2><span className='orange'>U-GUARD : </span> La solution &ldquo;Proximity&rdquo;</h2>
+                    <img className='solutions-img' src='/img/solutions/u-guard.png' />
+                    <p>5 capteurs (4 U-Guard-P et 1 U-Guard-M) génèrent un périmètre de sécurité autour de l&apos;engin afin de détecter la proximité des piétons ou d’autres engins (via un badge ou un système identique).</p>
+                    <p>Il existe
+                        <span> 2 zones de sécurité </span>
+                        controlées par le U-Guard-M :</p>
+                    <ul>
+                        <li>
+                            La
+                            <span className='yellow'> zone de prévention </span>
+                            qui informe le conducteur de la présence d&apos;autrui dans son espace de travail non visible.
+                        </li>
+                        <li>
+                            La
+                            <span className='red'> zone de danger </span>
+                            qui alerte les deux protagonistes d&apos;un danger de collision immédiat.
+                        </li>
+                    </ul>
+                </div>
 
-//                 <div className='content-col'>
-//                     <div className='column-4'>
-//                         <img src='/img/composants/u-guard/1.png'/>
-//                         <h5 className='red'>U-Guard-M</h5>
-//                         <ul>
-//                         <li>Synchronise les périphériques pour déterminer le périmètre de sécurité</li>
-//                         <li>Récupère les informations de distance reçus par les capteurs périphériques</li>
-//                         <li>Prend la décision d&apos;informer ou d&apos;alerter</li>
-//                         <li>Active les alerteurs</li>
-//                         </ul>
-//                     </div>
-//                     <div className='column-4'>
-//                     <img src='/img/composants/u-guard/2.png'/>
-//                         <h5 className='blue'>U-Guard-P</h5>
-//                         <ul>
-//                             <li>Mesure la distance avec les autres périphériques captés dans sa zone (engins et/ou piétons)</li>
-//                             <li>Communique les données de proximité au master</li>
-//                         </ul>
-//                     </div>
-//                     <div className='column-4'>
-//                     <img src='/img/composants/u-guard/3.png'/>
-//                          <h5 className='green'>U-Spot</h5>
-//                          <ul>
-//                              <li>Activé par la proximité d&apos;un engin</li>
-//                              <li>Mesure une distance complémentaire en cas de non visibilité</li>
-//                              <li>Communique les données de proximité au master</li>
-//                          </ul>
-//                     </div>
-//                     <div className='column-4'>
-//                     <img src='/img/composants/u-guard/4.png'/>
-//                         <h5 className='yellow'>U-Space</h5>
-//                         <ul>
-//                             <li>Répond aux demandes de mesures des capteurs périphériques</li>
-//                             <li>Prévient le piéton en cas de danger</li>
-//                             <li>Composant &ldquo;passif&rdquo;</li>
-//                         </ul>
-//                     </div>
-//                 </div>
-//             </section>
-//             <Footer/>
-//         </div>
-//     )
-// }
+            </section>
+
+            <section className='lavender-bg'>
+                <div className='margin'>
+                    <h3>Vos avantages</h3>
+                    <div className='content-col'>
+                        <div className='column-3 six'>
+                            <img className='picto' src='/img/pictos/u-guard/1.svg' />
+                            <p className='center'>Identifier et réduire les situations à risque sur votre site</p>
+                        </div>
+                        <div className='column-3 six'>
+                            <img className='picto' src='/img/pictos/u-guard/2.svg' />
+                            <p className='center'>Installer facilement et rapidement notre kit</p>
+                        </div>
+                        <div className='column-3 six'>
+                            <img className='picto' src='/img/pictos/u-guard/3.svg' />
+                            <p className='center'>Alerter le conducteur sur la proximité d&apos;un engin ou d&apos;un pieton</p>
+                        </div>
+                        <div className='column-3 six'>
+                            <img className='picto' src='/img/pictos/u-guard/4.svg' />
+                            <p className='center'>Améliorer les différents comportements de déplacement</p>
+                        </div>
+                        <div className='column-3 six'>
+                            <img className='picto' src='/img/pictos/u-guard/5.svg' />
+                            <p className='center'>Avertir les pietons lorsqu&apos;ils entrent dans une zone potentiellement risquée</p>
+                        </div>
+                        <div className='column-3 six'>
+                            <img className='picto' src='/img/pictos/u-guard/6.svg' />
+                            <p className='center'>Adapter les paramètres de détection à votre parc d&apos;engins et votre environnement</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='margin'>
+                <h3>Nos composants</h3>
+
+                <div className='content-col'>
+                    <div className='column-4'>
+                        <img src='/img/composants/u-guard/1.png' />
+                        <h5 className='red'>U-Guard-M</h5>
+                        <ul>
+                            <li>Synchronise les périphériques pour déterminer le périmètre de sécurité</li>
+                            <li>Récupère les informations de distance reçus par les capteurs périphériques</li>
+                            <li>Prend la décision d&apos;informer ou d&apos;alerter</li>
+                            <li>Active les alerteurs</li>
+                        </ul>
+                    </div>
+                    <div className='column-4'>
+                        <img src='/img/composants/u-guard/2.png' />
+                        <h5 className='blue'>U-Guard-P</h5>
+                        <ul>
+                            <li>Mesure la distance avec les autres périphériques captés dans sa zone (engins et/ou piétons)</li>
+                            <li>Communique les données de proximité au master</li>
+                        </ul>
+                    </div>
+                    <div className='column-4'>
+                        <img src='/img/composants/u-guard/3.png' />
+                        <h5 className='green'>U-Spot</h5>
+                        <ul>
+                            <li>Activé par la proximité d&apos;un engin</li>
+                            <li>Mesure une distance complémentaire en cas de non visibilité</li>
+                            <li>Communique les données de proximité au master</li>
+                        </ul>
+                    </div>
+                    <div className='column-4'>
+                        <img src='/img/composants/u-guard/4.png' />
+                        <h5 className='yellow'>U-Space</h5>
+                        <ul>
+                            <li>Répond aux demandes de mesures des capteurs périphériques</li>
+                            <li>Prévient le piéton en cas de danger</li>
+                            <li>Composant &ldquo;passif&rdquo;</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <Footer /> */}
+        </div>
+    )
+}
