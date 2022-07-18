1516afac5ba450f788368a576bee77206ea33cfc
Laura <Laurxding@hotmail.com>
2022-07-18 09:50:03 +0200

    add axeptio

---
+++ public/axeptio.jsx
@@ -0,0 +1,9 @@
+window.axeptioSettings = {
+  clientId: "62d4fe9b80344fcbce1de1d5",
+};
+ 
+(function(d, s) {
+  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+  e.async = true; e.src = "//static.axept.io/sdk-slim.js";
+  t.parentNode.insertBefore(e, t);
+})(document, "script");
