ee9457856eea5e8d320a33a77f2cd0115f1f2f70
Laurading <Laurxding@hotmail.com>
2022-07-20 11:35:53 +0200

    footer fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,21 +1,43 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useRouter } from "next/router";
+import * as locales from '../assets/locales';
+import Link from 'next/link';
+
 const Footer = () => {
+
+    const { locale } = useRouter();
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        setData(locales[locale])
+    },[locale])
+
     return (
-        <div className="footer">
+        <>
+        {data && (
+            <div className="footer">
             <div className="margin-lr">
                 <div className="content-col">
                     <div className="column-2 wrap">
                         <img src="img/footer/frenchtech.png" />
                         <img src="img/footer/offreur.png"/>
                     </div>
-                    <div className="column-2 wrap">
-                        <a className="cultured">Mentions Légales</a>
-                        <a className="cultured margin-left">© Copyright WizziLab</a>
+                    <div className="column-2 wrap space-between">
+                        <Link href="/privacypolicy">
+                            <a className="cultured">{data.footer.privacypolicy}</a>
+                        </Link>
+                        <Link href="/cgv">
+                            <a className="cultured">{data.footer.cgv}</a>
+                        </Link>
+                        <p className="cultured">© Copyright WizziLab</p>
                     </div>
                 </div>
 
             </div>
 
         </div>
+        )}
+        </>
     )
 
 }
